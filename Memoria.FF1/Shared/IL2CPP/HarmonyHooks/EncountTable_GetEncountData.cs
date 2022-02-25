5cbfc01b0b694663cef2840add5f628173aa7927
Albeoris <albeoris@gmail.com>
2022-02-25 17:49:00 +0300

    fix #24

--- Memoria.FF1/Shared/IL2CPP/HarmonyHooks/EncountTable_GetEncountData.cs
+++ Memoria.FF1/Shared/IL2CPP/HarmonyHooks/EncountTable_GetEncountData.cs
@@ -1,26 +1,24 @@
-﻿using HarmonyLib;
+﻿using System;
+using HarmonyLib;
+using Last.Data.Master;
 using Last.Map;
 using Memoria.FFPR.IL2CPP;
+using Serial.Template.Management;
 using Boolean = System.Boolean;
 using IntPtr = System.IntPtr;
 // ReSharper disable InconsistentNaming
 
 namespace Memoria.FFPR.IL2CPP.HarmonyHooks;
 
-[HarmonyPatch(typeof(EncounterLot), nameof(EncounterLot.CheckEncount))]
-public sealed class EncounterLot_CheckEncount : Il2CppSystem.Object
+[HarmonyPatch(typeof(EncountTable), nameof(EncountTable.GetEncountData))]
+public static class EncountTable_GetEncountData
 {
-    // ReSharper disable once IdentifierTypo
-    public EncounterLot_CheckEncount(IntPtr ptr) : base(ptr)
-    {
-    }
-
-    public static Boolean Prefix(ref Boolean __result)
+    public static Boolean Prefix(ref MonsterParty __result)
     {
         if (ModComponent.Instance.EncountersControl.DisableEncounters)
         {
-            __result = false;
-                
+            __result = null;
+
             // Skip native method
             return false;
         }
