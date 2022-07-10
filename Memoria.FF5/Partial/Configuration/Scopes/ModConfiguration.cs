1a55ec497da7e290a5417e5e1cff3ecec9bdff04
Albeoris <albeoris@gmail.com>
2022-07-10 13:30:23 +0300

    FF5: Disable Texture Import

--- Memoria.FF5/Partial/Configuration/Scopes/ModConfiguration.cs
+++ Memoria.FF5/Partial/Configuration/Scopes/ModConfiguration.cs
@@ -1,9 +1,15 @@
-﻿namespace Memoria.FFPR.Configuration.Scopes;
+﻿using Memoria.FFPR.IL2CPP;
+
+namespace Memoria.FFPR.Configuration.Scopes;
 
 public sealed partial class ModConfiguration
 {
     private partial void InitializeGameSpecificOptions(ConfigFileProvider provider)
     {
-        
+        if (Assets.ImportTextures)
+        {
+            Assets.DisableImportTextures();
+            ModComponent.Log.LogWarning("Texture import is not supported: https://github.com/Albeoris/Memoria.FFPR/issues/37");
+        }
     }
 }
