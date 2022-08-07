00e04e87e7dbed550b3b176c44c16a3bf801529a
Albeoris <albeoris@gmail.com>
2022-08-07 10:55:11 +0300

    fix #28 - disable encounters on Veldt

--- Memoria.FFPR/Shared/IL2CPP/HarmonyHooks/EncountTable_GetEncountData.cs
+++ Memoria.FFPR/Shared/IL2CPP/HarmonyHooks/EncountTable_GetEncountData.cs
@@ -6,28 +6,10 @@ using Memoria.FFPR.IL2CPP;
 using Serial.Template.Management;
 using Boolean = System.Boolean;
 using IntPtr = System.IntPtr;
-// ReSharper disable InconsistentNaming
 
 namespace Memoria.FFPR.IL2CPP.HarmonyHooks;
 
-// [HarmonyPatch(typeof(EncounterLot), nameof(EncounterLot.StandingInExclusionArea))]
-// public static class EncounterLot_StandingInExclusionArea
-// {
-//     public static Boolean Prefix(ref MonsterParty __result)
-//     {
-//         if (ModComponent.Instance.EncountersControl.DisableEncounters)
-//         {
-//             __result = null;
-//
-//             // Skip native method
-//             return false;
-//         }
-//
-//         // Call native method
-//         return true;
-//     }
-// }
-
+// ReSharper disable InconsistentNaming
 [HarmonyPatch(typeof(EncountTable), nameof(EncountTable.GetEncountData))]
 public static class EncountTable_GetEncountData
 {
---
+++ Memoria.FFPR/Shared/IL2CPP/HarmonyHooks/EncounterLot_StandingInExclusionArea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HarmonyLib;
+using Last.Map;
+
+namespace Memoria.FFPR.IL2CPP.HarmonyHooks;
+
+// ReSharper disable InconsistentNaming
+[HarmonyPatch(typeof(EncounterLot), nameof(EncounterLot.StandingInExclusionArea))]
+public static class EncounterLot_StandingInExclusionArea
+{
+    public static Boolean Prefix(ref Boolean __result)
+    {
+        if (ModComponent.Instance.EncountersControl.DisableEncounters)
+        {
+            __result = true;
+
+            // Skip native method
+            return false;
+        }
+
+        // Call native method
+        return true;
+    }
+}
