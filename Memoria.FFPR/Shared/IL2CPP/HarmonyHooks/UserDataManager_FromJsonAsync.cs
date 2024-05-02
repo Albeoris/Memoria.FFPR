6c4d645ff8f4012a78d12de8603bbc48964a0a1c
Albeoris <albeoris@gmail.com>
2024-05-02 05:43:41 +0200

    fix #48, #49

--- Memoria.FFPR/Shared/IL2CPP/HarmonyHooks/UserDataManager_FromJsonAsync.cs
+++ Memoria.FFPR/Shared/IL2CPP/HarmonyHooks/UserDataManager_FromJsonAsync.cs
@@ -6,18 +6,21 @@ using Memoria.FFPR.BeepInEx;
 namespace Memoria.FFPR.IL2CPP.HarmonyHooks;
 
 // ReSharper disable InconsistentNaming
-[HarmonyPatch(typeof(UserDataManager), nameof(UserDataManager.FromJsonAsync))]
-public static class UserDataManager_FromJsonAsync
-{
-    public static void Postfix(String json)
-    {
-        try
-        {
-            SaveManager.LoadData(json);
-        }
-        catch (Exception ex)
-        {
-            ModComponent.Log.LogException(ex);
-        }
-    }
-}
+
+// Handling FromJsonAsync crashes the game :/ 
+
+// [HarmonyPatch(typeof(UserDataManager), nameof(UserDataManager.FromJsonAsync))]
+// public static class UserDataManager_FromJsonAsync
+// {
+//     public static void Postfix(String json)
+//     {
+//         try
+//         {
+//             SaveManager.LoadData(json);
+//         }
+//         catch (Exception ex)
+//         {
+//             ModComponent.Log.LogException(ex);
+//         }
+//     }
+// }
